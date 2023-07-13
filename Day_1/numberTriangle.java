a8c21eb3f9a23a57be5ff122285f9f9c151caa31
AngelinJenifer <139155866+AngelinJenifer@users.noreply.github.com>
2023-07-13 15:07:50 +0530

    Add files via upload

---
+++ Day_1/numberTriangle.java
@@ -0,0 +1,18 @@
+class numberTriangle {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            for(int k=1;k<=n-1;k++)
+            {
+                System.out.print(" ");
+                
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
